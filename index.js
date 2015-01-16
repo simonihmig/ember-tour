ca3acef552aaf54e148a03f963233f4b242f3d1c
jondayton <jondayton@gmail.com>
2015-01-16 15:28:41 -0500

    add intro.js

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-tour'
+  name: 'ember-tour',
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/intro.js/intro.js');
+  }
 };
