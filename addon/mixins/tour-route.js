f9ffb0ee5c85fcc376389d1db357924a9f587085
jondayton <jondayton@gmail.com>
2015-02-21 18:21:10 -0500

    search the route tree to find an appropriate tour

--- addon/mixins/tour-route.js
+++ addon/mixins/tour-route.js
@@ -1,6 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
+  findRouteTourInTours: function(tours, route) {
+    var controller = this.controllerFor("application");
+    var searchedRoute = route || controller.get('currentRouteName');
+    var tour = tours.findBy('name', searchedRoute);
+    if (Ember.isBlank(tour)) {
+      var nextRoute = searchedRoute.split('.').slice(0, -1).join('.');
+      if (Ember.isBlank(nextRoute)) {
+        return null;
+      } else {
+        return this.findRouteTourInTours(tours, nextRoute);
+      }
+    } else {
+      return tour;
+    }
+  },
+
   _actions: {
     startTour: function(name, startingStep) {
       var route = this;
