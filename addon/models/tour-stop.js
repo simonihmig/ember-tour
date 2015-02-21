a9d8e0f5f1ce4b253c76bf6b2ba851a5f0cc2d70
jondayton <jondayton@gmail.com>
2015-02-21 18:47:16 -0500

    cleanup unused properties

--- addon/models/tour-stop.js
+++ addon/models/tour-stop.js
@@ -3,8 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   intro: DS.attr('string'),
   step: DS.attr('number'),
-  tooltipClass: DS.attr('string'),
-  highlightClass: DS.attr('string'),
   position: DS.attr('string'),
   element: DS.attr('string'),
   targetRoute: DS.attr('string'),
--- addon/models/tour.js
+++ addon/models/tour.js
@@ -2,17 +2,11 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  overlayOpacity: DS.attr('number'),
-  exitOnEsc: DS.attr('boolean', {defaultValue: true}),
   spotlight: DS.attr('boolean', {defaultValue: true}),
-  exitOnOverlayClick: DS.attr('boolean', {defaultValue: true}),
   showStepNumbers: DS.attr('boolean', {defaultValue: true}),
-  keyboardNavigation: DS.attr('boolean', {defaultValue: true}),
-  showButtons: DS.attr('boolean', {defaultValue: true}),
-  showNumber: DS.attr('boolean', {defaultValue: true}),
   showBullets: DS.attr('boolean', {defaultValue: true}),
-  showProgress: DS.attr('boolean', {defaultValue: true}),
   scrollToElement: DS.attr('boolean', {defaultValue: true}),
-  disableInteraction: DS.attr('boolean'),
   tourStops: DS.hasMany('tourStop')
+  //exitOnOverlayClick: DS.attr('boolean', {defaultValue: true}),
+  //disableInteraction: DS.attr('boolean'),
 });
