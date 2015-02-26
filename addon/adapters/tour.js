2835f663a9cebf462055ac37f3b830f14c7491dd
jondayton <jondayton@gmail.com>
2015-02-25 22:39:48 -0500

    add adapter shell

---
+++ addon/adapters/tour.js
@@ -0,0 +1,26 @@
+import DS from 'ember-data';
+
+export default DS.Adapter.extend({
+  find: function(store, type, id, record) {
+
+  },
+  findAll: function(store, type, sinceToken) {
+
+  },
+  findQuery: function(store, type, query) {
+
+  },
+  findMany: function(store, type, ids, records) {
+
+  },
+
+  findHasMany: function(store, record, url, relationship) {
+
+  },
+  findBelongsTo: function(store, record, url, relationship) {
+
+  },
+  pathForType: function(type) {
+
+  }
+});
